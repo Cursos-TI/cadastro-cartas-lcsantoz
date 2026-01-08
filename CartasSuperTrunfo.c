7d072a4f5bcfbc85628f75848553c6e8d4039283
lcsantoz <lc.07.santoss@gmail.com>
2026-01-08 02:05:46 +0000

    Correção da ausência do &

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -24,7 +24,7 @@ int pontos2;
   //Entrada de Dados
   //Carta 01
   printf("Digite o Estado: \n");
-  scanf(" %c", estado1);
+  scanf(" %c", &estado1);
 
   printf("Digite o código: \n");
   scanf("%s", codigo1);
@@ -33,20 +33,20 @@ int pontos2;
   scanf("%s", cidade1);
 
   printf("Digite a população: \n");
-  scanf("%d", populaçao1);
+  scanf("%d", &populaçao1);
 
   printf("Digite a área: \n");
-  scanf("%f", area1);
+  scanf("%f", &area1);
 
   printf("Digite o  PIB: \n ");
-  scanf("%f", pib1);
+  scanf("%f", &pib1);
 
   printf("Digite os Pontos Turísticos: \n");
-  scanf("%d", pontos1);
+  scanf("%d", &pontos1);
 
   //Carta 02
   printf("Digite o Estado: \n");
-  scanf(" %c", estado2);
+  scanf(" %c", &estado2);
 
   printf("Digite o código: \n");
   scanf("%s", codigo2);
@@ -55,16 +55,16 @@ int pontos2;
   scanf("%s", cidade2);
 
   printf("Digite a população: \n");
-  scanf("%d", populaçao2);
+  scanf("%d", &populaçao2);
 
   printf("Digite a área: \n");
-  scanf("%f", area2);
+  scanf("%f", &area2);
 
   printf("Digite o  PIB: \n ");
-  scanf("%f", pib2);
+  scanf("%f", &pib2);
 
   printf("Digite os Pontos Turísticos: \n");
-  scanf("%d", pontos2);
+  scanf("%d", &pontos2);
 
 
   //Exibição dos dados da cidade
@@ -76,7 +76,7 @@ int pontos2;
   printf("População: %d \n", populaçao1);
   printf("Área: %f \n", area1);
   printf("PIB: %f \n", pib1);
-  printf("Pontos turísticos: %d \n", pontos1)
+  printf("Pontos turísticos: %d \n", pontos1);
 
   //Carta 02
   printf("Carta 02 \n");
@@ -86,7 +86,7 @@ int pontos2;
   printf("População: %d \n", populaçao2);
   printf("Área: %f \n", area2);
   printf("PIB: %f \n", pib2);
-  printf("Pontos turísticos: %d \n", pontos2)
+  printf("Pontos turísticos: %d \n", pontos2);
   
 
 
